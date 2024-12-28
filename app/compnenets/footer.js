1dccc9b61dd9e5f640dd71ab82e67ca8eb43f3cb
Ambika Prasad <akashboard2021@gmail.com>
2024-12-28 15:46:04 +0530

    #project initialization #created footer

---
+++ app/compnenets/footer.js
@@ -0,0 +1,19 @@
+import styles from "./footer.module.css";
+
+export default function Footer() {
+  return (
+    <div className={styles.footer}>
+      <p className={styles.f_h}>Get In Touch</p>
+      <p className={styles.f_t}>
+        I'm currently looking for full-time React Developer/Frontend Developer
+        opportunities! If you know of any positions available, if you have any
+        questions, or if you just want to say hi, please feel free to email me
+        at, <span className={styles.mail}>khansaif59@gmail.com</span>.
+      </p>
+      <div className={styles.f_b}>
+        🧑‍💻 with 💖 by <span className={styles.f_n}>Mohd Shahid Iqbal</span>{" "}
+        using ⚛️
+      </div>
+    </div>
+  );
+}
