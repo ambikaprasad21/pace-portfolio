137a6a22ea6cc1e0420b012cfc5e9b9dc0bd34f4
Ambika Prasad <akashboard2021@gmail.com>
2024-12-29 10:24:34 +0530

    #completed

--- app/compnenets/hero.js
+++ app/compnenets/hero.js
@@ -5,40 +5,43 @@ import styles from "./hero.module.css";
 
 export default function Hero() {
   return (
-    <div className={styles.container} id="home">
-      <div className={styles.intro}>
-        <h1>Mohd Shahid Iqbal</h1>
-        {/* <p>Passionate about changing the world with technology.</p> */}
-        <TypewriterComponent />
+    <>
+      <div id="stars"></div>
+      <div className={styles.container} id="home">
+        <div className={styles.intro}>
+          <h1>Mohd Shahid Iqbal</h1>
+          {/* <p>Passionate about changing the world with technology.</p> */}
+          <TypewriterComponent />
+        </div>
+        <div className={styles.social}>
+          <a href="https://github.com/Mohd-shahid-iqbal" target="_blank">
+            <i className="fab fa-github  fa-3x socialicons"></i>
+          </a>
+
+          <a
+            target="_blank"
+            href="https://www.facebook.com/profile.php?id=100002142394297"
+            aria-label="My facebook"
+          >
+            <i className="fab fa-facebook  fa-3x socialicons"></i>
+          </a>
+
+          <a target="_blank" href="https://www.instagram.com/shahid27945/">
+            <i className="fab fa-instagram  fa-3x socialicons"></i>
+          </a>
+
+          <a
+            target="_blank"
+            href="https://www.linkedin.com/in/mohd-shahid-iqbal-2b690713b/"
+          >
+            <i className="fab fa-linkedin  fa-3x socialicons"></i>
+          </a>
+        </div>
+        <button className={styles.btn}>
+          <a href="#aboutme">More about me</a>
+        </button>
       </div>
-      <div className={styles.social}>
-        <a href="https://github.com/Mohd-shahid-iqbal" target="_blank">
-          <i className="fab fa-github  fa-3x socialicons"></i>
-        </a>
-
-        <a
-          target="_blank"
-          href="https://www.facebook.com/profile.php?id=100002142394297"
-          aria-label="My facebook"
-        >
-          <i className="fab fa-facebook  fa-3x socialicons"></i>
-        </a>
-
-        <a target="_blank" href="https://www.instagram.com/shahid27945/">
-          <i className="fab fa-instagram  fa-3x socialicons"></i>
-        </a>
-
-        <a
-          target="_blank"
-          href="https://www.linkedin.com/in/mohd-shahid-iqbal-2b690713b/"
-        >
-          <i className="fab fa-linkedin  fa-3x socialicons"></i>
-        </a>
-      </div>
-      <button className={styles.btn}>
-        <a href="#aboutme">More about me</a>
-      </button>
-    </div>
+    </>
   );
 }
 
